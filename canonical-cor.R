cf1548e1d4ecfe577a809c9bd0fb87f4d27d07f4
bayupaoh <unikom.codelabs@gmail.com>
2017-05-03 23:17:22 +0700

    update canonical corelation

--- canonical-cor.R
+++ canonical-cor.R
@@ -1,20 +1,20 @@
 getwd();
 setwd("~/Desktop/skripsi_latihan_canonicalcorelation/");
 #install library
-update.packages()
+#update.packages()
 install.packages("CCA")
 library(CCA)
 
 #ambil data
 data <- read.csv("data-skripsi.csv",sep = ",")
-colnames(data)<- c("suhu", "kelembapan", "amonia", "kematian", "ip", "hargajual")
-kondisiling<- data[,1:4];
+#colnames(data)<- c("suhu", "kelembapan", "amonia", "kematian", "ip", "hargajual")
+kondisiling<- data[,1:3];
 kualitasayam <- data[,5:6];
 
 #cari matiks korelasi
 matcor<-matcor(kondisiling,kualitasayam)
-matcorxy <- matcor$XYcor[1:4,5:6]
-matcoryx <- matcor$XYcor[5:6,1:4]
+matcorxy <- matcor$XYcor[1:3,4:5]
+matcoryx <- matcor$XYcor[4:5,1:3]
 
 #ambil nilai lambda dan bi
 bi <- eigen(solve(matcor$Ycor)%*%matcoryx%*%solve(matcor$Xcor)%*%matcorxy)
@@ -28,11 +28,10 @@ max<-which.max(lamda)
 n <- dim(kondisiling)[1]
 p <- length(kondisiling)
 q <- length(kualitasayam)
-
 hasil <- (-1*(n-0.5*(p+q+1)))*(sum(log(1-lamda)))
 #menggunakan loading canonical
 #cor(x,ui)
 corxui<-matcor$Xcor%*%ai[,max]
 #cor(y,vi)
-coryvi<-matcor$Ycor%*%bi$vectors[,max]
+coryvi<-matcor$Ycor%*%bi$vectors[max,]
 
